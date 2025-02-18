6af4c25002f87ee3867bd1ce2da20c5e7aacccb9
Mohamed Mouhoud <tahamou@udel.edu>
2025-02-17 19:03:03 -0500

    MainCameraController Updated + Hello World changed into a joke.

---
+++ Assets/MainCameraController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Test: Convert 1010 to binary. Me: Sorry I dont do binaries");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
